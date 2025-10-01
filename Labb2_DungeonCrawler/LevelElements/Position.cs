26e9fd599488f47a03f92dc020e5f0241a9310e3
Robin Hermansson <robin.hermansson@stendahls.se>
2025-10-01 16:34:32 +0200

    Add project files.

---
+++ Labb2_DungeonCrawler/LevelElements/Position.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb2_DungeonCrawler.LevelElements
+{
+    public struct Position
+    {
+
+        public int XPos { get; set; }
+        public int YPos { get; set; }
+        public Position(int xpos, int ypos)
+        {
+            XPos = xpos;
+            YPos = ypos;
+        }
+    }
+}
