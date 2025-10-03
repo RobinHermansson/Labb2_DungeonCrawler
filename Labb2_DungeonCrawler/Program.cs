fb5854755c5179ca9abbf0b664f04cc399723f1a
Robin Hermansson <robin.hermansson@stendahls.se>
2025-10-03 05:41:20 +0200

    clean up unused code

--- Labb2_DungeonCrawler/Program.cs
+++ Labb2_DungeonCrawler/Program.cs
@@ -26,59 +26,7 @@ while (true)
     {
         player.MoveTo(attempt);
     }
-    /*
-    switch (attempt)
-    {
-        case ConsoleKey.W:
-            if (player.AttemptMove(DirectionTransformer.GetPositionDelta(Direction.Up) + player.Position, gameState))
-            {
-                player.MoveTo((DirectionTransformer.GetPositionDelta(Direction.Up) + player.Position));
-            }
-            break;
-        case ConsoleKey.S:
-            if (player.AttemptMove(DirectionTransformer.GetPositionDelta(Direction.Down) + player.Position, gameState))
-            {
-                player.MoveTo((DirectionTransformer.GetPositionDelta(Direction.Down) + player.Position));
-            }
-            break;
-        case ConsoleKey.A:
-            if (player.AttemptMove(DirectionTransformer.GetPositionDelta(Direction.Left) + player.Position, gameState))
-            {
-                player.MoveTo((DirectionTransformer.GetPositionDelta(Direction.Left) + player.Position));
-            }
-            break;
-        case ConsoleKey.D:
-            if (player.AttemptMove(DirectionTransformer.GetPositionDelta(Direction.Right) + player.Position, gameState))
-            {
-                player.MoveTo((DirectionTransformer.GetPositionDelta(Direction.Right) + player.Position));
-            }
-            break;
-        case ConsoleKey.UpArrow:
-            if (player.AttemptMove(DirectionTransformer.GetPositionDelta(Direction.Up) + player.Position, gameState))
-            {
-                player.MoveTo(DirectionTransformer.GetPositionDelta(Direction.Up) + player.Position);
-            }
-            break;
-        case ConsoleKey.DownArrow:
-            if (player.AttemptMove(DirectionTransformer.GetPositionDelta(Direction.Down) + player.Position, gameState))            
-            {
-                player.MoveTo(DirectionTransformer.GetPositionDelta(Direction.Down) + player.Position);
-            }
-            break;
-        case ConsoleKey.LeftArrow:
-            if (player.AttemptMove(DirectionTransformer.GetPositionDelta(Direction.Left) + player.Position, gameState))
-            {
-                player.MoveTo((DirectionTransformer.GetPositionDelta(Direction.Left) + player.Position));
-            }
-            break;
-        case ConsoleKey.RightArrow:
-            if (player.AttemptMove(DirectionTransformer.GetPositionDelta(Direction.Right) + player.Position, gameState))
-            {
-                player.MoveTo((DirectionTransformer.GetPositionDelta(Direction.Right) + player.Position));
-            }
-            break;
-    }*/
-    // TODO: Create a gameloop..
+        // TODO: Create a gameloop..
     player.Draw();
 }
 
