46437bd0afb3e82c3f1a1b236ebd5ea6bc297a3b
Robin Hermansson <robin.hermansson@stendahls.se>
2025-10-03 06:31:51 +0200

    attempt at a Gameloop

---
+++ Labb2_DungeonCrawler/Core/Gameloop.cs
@@ -0,0 +1,54 @@
+﻿using Labb2_DungeonCrawler.LevelElements;
+namespace Labb2_DungeonCrawler.Core
+{
+    public class Gameloop
+    {
+        public GameState GameState{ get; set; }
+        public Player Player { get; set; }
+        public Gameloop(GameState gameState)
+        {
+
+            GameState = gameState;
+            Player = gameState.Player;
+
+        }
+
+        public void PlayGame()
+        {
+            foreach (var element in GameState.LevelData.LevelElementsList)
+            {
+                element.Draw();
+            }
+
+            Console.CursorVisible = false;
+            while (true)
+            {
+                
+                ConsoleKeyInfo input = Console.ReadKey();
+                Position oldPosition = Player.Position;
+                Console.SetCursorPosition(oldPosition.XPos, oldPosition.YPos);
+                Console.Write(' ');
+
+                Position attempt = Player.MovementHandler(input);
+                if (Player.AttemptMove(attempt, GameState))
+                {
+                    Player.MoveTo(attempt);
+                }
+                Player.Draw();
+
+                foreach (var element in GameState.LevelData.LevelElementsList)
+                {
+                    if (element is Rat enemyRat)
+                    {
+                        oldPosition = enemyRat.Position;
+                        Console.SetCursorPosition(oldPosition.XPos, oldPosition.YPos);
+                        Console.Write(' ');
+                        enemyRat.Update();
+                        enemyRat.Draw();
+                    }
+                }
+
+            }
+        }
+    }
+}
--- Labb2_DungeonCrawler/Program.cs
+++ Labb2_DungeonCrawler/Program.cs
@@ -7,39 +7,7 @@ using Labb2_DungeonCrawler.Utilities;
 GameState gameState = new GameState();
 Player player = gameState.Player;
 Console.CursorVisible = false;
-foreach (var element in gameState.LevelData.LevelElementsList)
-{
-    element.Draw();
-}
 
-Console.CursorVisible = false;
-while (true)
-{
-    
-    ConsoleKeyInfo input = Console.ReadKey();
-    Position oldPosition = player.Position;
-    Console.SetCursorPosition(oldPosition.XPos, oldPosition.YPos);
-    Console.Write(' ');
+Gameloop gameLoop = new Gameloop(gameState);
 
-    Position attempt = player.MovementHandler(input);
-    if (player.AttemptMove(attempt, gameState))
-    {
-        player.MoveTo(attempt);
-    }
-        // TODO: Create a gameloop..
-    player.Draw();
-
-    foreach (var element in gameState.LevelData.LevelElementsList)
-    {
-        if (element is Rat enemyRat)
-        {
-            oldPosition = enemyRat.Position;
-            Console.SetCursorPosition(oldPosition.XPos, oldPosition.YPos);
-            Console.Write(' ');
-            enemyRat.Update();
-            enemyRat.Draw();
-        }
-    }
-
-}
-
+gameLoop.PlayGame();
