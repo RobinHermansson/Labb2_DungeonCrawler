d6c6bf880a82285c1beedbcaa8730c9334c25fb6
Robin Hermansson <robin.hermansson@stendahls.se>
2025-10-03 05:42:54 +0200

    Add needed paramater to interface MoveTo

--- Labb2_DungeonCrawler/LevelElements/IMovable.cs
+++ Labb2_DungeonCrawler/LevelElements/IMovable.cs
@@ -5,5 +5,5 @@ namespace Labb2_DungeonCrawler.LevelElements;
 public interface IMovable
 {
     public bool AttemptMove(Position position, GameState gameState);
-    public void MoveTo();
+    public void MoveTo(Position position);
 }
