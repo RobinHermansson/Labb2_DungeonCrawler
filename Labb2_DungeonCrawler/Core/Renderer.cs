65dbffd0d0a8732240b72143dc23b2831acbadd1
Robin Hermansson <robin.hermansson@stendahls.se>
2025-10-07 08:21:20 +0200

    Added a Title screen

--- Labb2_DungeonCrawler/Core/Renderer.cs
+++ Labb2_DungeonCrawler/Core/Renderer.cs
@@ -70,4 +70,99 @@ public class Renderer
         Console.SetCursorPosition(0, 10);
         Console.WriteLine(); 
     }
+    public void DisplayTitleScreen(StartScreenOption selection)
+    {
+        int height = 16;
+        int width = 32;
+        int startX = 0;     
+        int startY = 0;
+
+        string titleText = "ABANDONED CELLAR";
+        string startSelectionText = "Start game";
+        string quitGameText = "Run away";
+        
+        
+        Console.ForegroundColor = ConsoleColor.DarkRed; 
+        
+        // Draw the top border
+        Console.SetCursorPosition(startX, startY);
+        Console.Write("╔");
+        for (int i = 1; i < width - 1; i++)
+            Console.Write("═");
+        Console.Write("╗");
+        
+        // Draw the sides
+        for (int i = 1; i < height - 1; i++)
+        {
+            Console.SetCursorPosition(startX, startY + i);
+            Console.Write("║");
+            Console.SetCursorPosition(startX + width - 1, startY + i);
+            Console.Write("║");
+        }
+        
+        // Draw the bottom border
+        Console.SetCursorPosition(startX, startY + height - 1);
+        Console.Write("╚");
+        for (int i = 1; i < width - 1; i++)
+            Console.Write("═");
+        Console.Write("╝");
+
+        // Write all text centered but with some offets 
+        int titleYOffset = 3; 
+        int titleX = startX + (width - titleText.Length) / 2;
+        int titleY = (startY + height / 2) - titleYOffset;
+        Console.SetCursorPosition(titleX, titleY);
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.Write(titleText);
+        Console.ResetColor();
+        
+        int startGameX = startX + (width - startSelectionText.Length) / 2;
+        int startGameY = startY +  height / 2;
+        Console.SetCursorPosition(startGameX, startGameY);
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write(startSelectionText);
+        Console.ResetColor();
+
+        int quitGameYOffset = 1;
+        int quitGameXOffset = 1;
+        int quitGameX = (startX + (width - quitGameText.Length) - quitGameXOffset) / 2;
+        int quitGameY = startY + quitGameYOffset +  height / 2;
+        Console.SetCursorPosition(quitGameX, quitGameY);
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write(quitGameText);
+        Console.ResetColor();
+
+        
+        
+        if (selection == StartScreenOption.Start)
+        {
+            Console.SetCursorPosition(startGameX -3, startGameY);
+            Console.Write('=');
+            Console.SetCursorPosition(startGameX -2, startGameY);
+            Console.Write('>');
+            Console.SetCursorPosition(quitGameX -3, quitGameY);
+            Console.Write(' ');
+            Console.SetCursorPosition(quitGameX -2, quitGameY);
+            Console.Write(' ');
+        }
+        else
+        {
+            Console.SetCursorPosition(startGameX -3, startGameY);
+            Console.Write(' ');
+            Console.SetCursorPosition(startGameX -2, startGameY);
+            Console.Write(' ');
+            Console.SetCursorPosition(quitGameX -3, quitGameY);
+            Console.Write('=');
+            Console.SetCursorPosition(quitGameX -2, quitGameY);
+            Console.Write('>');   
+        }
+        
+        Console.SetCursorPosition(0, height);
+        Console.WriteLine();
+    }
+    public enum StartScreenOption
+    {
+        Start,
+        Quit
+    }
 }
--- Labb2_DungeonCrawler/Program.cs
+++ Labb2_DungeonCrawler/Program.cs
@@ -5,6 +5,36 @@
 GameState gameState = new GameState();
 Console.CursorVisible = false;
 
-Gameloop gameLoop = new Gameloop(gameState);
+Renderer renderer = new Renderer();
+Renderer.StartScreenOption selectedOption = Renderer.StartScreenOption.Start;
 
-gameLoop.PlayGame();
+while (true)
+{
+    renderer.DisplayTitleScreen(selectedOption);
+    var input = Console.ReadKey();
+    switch (input.Key)
+    {
+        case ConsoleKey.UpArrow:
+            if (selectedOption == Renderer.StartScreenOption.Quit)
+                selectedOption = Renderer.StartScreenOption.Start;
+            break;
+            
+        case ConsoleKey.DownArrow:
+            if (selectedOption == Renderer.StartScreenOption.Start)
+                selectedOption = Renderer.StartScreenOption.Quit;
+            break;
+            
+        case ConsoleKey.Enter:
+            if (selectedOption == Renderer.StartScreenOption.Start)
+            {
+                Console.Clear();
+                Gameloop gameLoop = new Gameloop(gameState);
+                gameLoop.PlayGame();
+            }
+            else
+                Environment.Exit(0);
+            break;
+    }
+}
+
+
