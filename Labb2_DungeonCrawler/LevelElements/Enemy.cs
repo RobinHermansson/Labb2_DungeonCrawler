536613f37f1105ba09b5a096249e9265deda1628
Robin Hermansson <robin.hermansson@hotmail.se>
2025-10-08 08:42:35 +0200

    Remove unused Attack

--- Labb2_DungeonCrawler/LevelElements/Enemy.cs
+++ Labb2_DungeonCrawler/LevelElements/Enemy.cs
@@ -10,37 +10,5 @@ public abstract class Enemy : Character
     }
 
     public abstract void Update();
-    public bool Attack(Character target)
-    {
-        int attackRollTotal = 0;
-        int defenceRollTotal = 0;
-        foreach (Dice dice in this.AttackDice)
-        {
-            int roll = dice.Roll();
-            attackRollTotal += roll;
-
-        }
-        Console.WriteLine($"{this.Name} rolled his {this.AttackDiceCount}d6+{this.AttackModifier}. {this.Name}'s Attacktotal is: {attackRollTotal}+{this.AttackModifier} ({attackRollTotal + this.AttackModifier})");
-
-        foreach (Dice dice in target.DefenceDice)
-        {
-            int roll = dice.Roll();
-            defenceRollTotal += roll;
-        }
-        Console.WriteLine($"{target.Name} rolled his {this.DefenceDiceCount}d6+{target.DefenceModifier}. {target.Name}'s Defence total is: {defenceRollTotal}+{target.DefenceModifier} ({defenceRollTotal + target.DefenceModifier})");
-        if ((attackRollTotal + AttackModifier) > (defenceRollTotal + target.DefenceModifier))
-        {
-            int totalDamageTaken = (attackRollTotal + this.AttackModifier) - (defenceRollTotal + target.DefenceModifier);
-            Console.WriteLine($"{target.Name} is about to take {totalDamageTaken}!");
-            target.TakeDamage(totalDamageTaken);
-            return true;
-        }
-        else
-        {
-            Console.WriteLine($"{this.Name} misses {target.Name}");
-            return false;
-        }
-    }
-
 }
 
