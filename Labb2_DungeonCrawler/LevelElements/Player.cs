81810a8a6a8e5d2a4a1c2563507d0f28a22c789d
Robin Hermansson <robin.hermansson@hotmail.se>
2025-10-08 08:40:19 +0200

    cleanup unused code

--- Labb2_DungeonCrawler/LevelElements/Player.cs
+++ Labb2_DungeonCrawler/LevelElements/Player.cs
@@ -27,10 +27,6 @@ public class Player : Character
             DefenceDice.Add(new Dice());
         }
     }
-    public void Update()
-    {
-    }
-
     public bool AttemptMove(Position attempt, GameState gameState)
     {
         return gameState.IsPositionWalkable(attempt) ? true : false;
@@ -39,10 +35,6 @@ public class Player : Character
     public void MoveTo(Position position)
     {
         this.Position = position;
-
-    }
-    public void MoveMe(Direction direction)
-    {
 
     }
 
@@ -90,37 +82,6 @@ public class Player : Character
                 throw new ArgumentException("Could not handle the input key.");
         }
 
-    }
-    public bool Attack(Character target)
-    {
-        int attackRollTotal = 0;
-        int defenceRollTotal = 0;
-        foreach (Dice dice in this.AttackDice)
-        {
-            int roll = dice.Roll();
-            attackRollTotal += roll;
-
-        }
-        Console.WriteLine($"{this.Name} rolled his {this.AttackDiceCount}d6+{this.AttackModifier}. {this.Name}'s Attacktotal is: {attackRollTotal}+{this.AttackModifier} ({attackRollTotal + this.AttackModifier})");
-
-        foreach (Dice dice in target.DefenceDice)
-        {
-            int roll = dice.Roll();
-            defenceRollTotal += roll;
-        }
-        Console.WriteLine($"{target.Name} rolled his {this.DefenceDiceCount}d6+{target.DefenceModifier}. {target.Name}'s Defence total is: {defenceRollTotal}+{target.DefenceModifier} ({defenceRollTotal + target.DefenceModifier})");
-        if ((attackRollTotal + AttackModifier) > (defenceRollTotal + target.DefenceModifier))
-        {
-            int totalDamageTaken = (attackRollTotal + this.AttackModifier) - (defenceRollTotal + target.DefenceModifier);
-            Console.WriteLine($"{target.Name} is about to take {totalDamageTaken}!");
-            target.TakeDamage(totalDamageTaken);
-            return true;
-        }
-        else
-        {
-            Console.WriteLine($"{this.Name} misses {target.Name}");
-            return false;
-        }
     }
     public void TakeDamage(int damage)
     {
