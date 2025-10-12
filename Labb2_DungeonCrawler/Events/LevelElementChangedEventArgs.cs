4a2bbfbc93a3fa9ff5c644082ec32acbf69bcb42
Robin Hermansson <robin.hermansson@stendahls.se>
2025-10-12 08:25:16 +0200

    Start of event setup

---
+++ Labb2_DungeonCrawler/Events/LevelElementChangedEventArgs.cs
@@ -0,0 +1,29 @@
+﻿using Labb2_DungeonCrawler.LevelElements;
+namespace Labb2_DungeonCrawler.Events;
+
+public class LevelElementChangedEventArgs : EventArgs
+{
+    public Position OldPosition { get; }
+    public Position NewPosition { get; }
+    public bool VisibilityChanged { get; }
+    public bool IsAliveChanged { get; }
+    
+    // Constructor for position changes
+    public LevelElementChangedEventArgs(Position oldPosition, Position newPosition)
+    {
+        OldPosition = oldPosition;
+        NewPosition = newPosition;
+        VisibilityChanged = false;
+        IsAliveChanged = false;
+    }
+    
+    // Constructor for visibility or state changes
+    public LevelElementChangedEventArgs(bool visibilityChanged, bool isAliveChanged)
+    {
+        VisibilityChanged = visibilityChanged;
+        IsAliveChanged = isAliveChanged;
+        // Same position for non-movement changes
+        //OldPosition = null;
+        //NewPosition = null;
+    }
+}
--- Labb2_DungeonCrawler/LevelElements/LevelElement.cs
+++ Labb2_DungeonCrawler/LevelElements/LevelElement.cs
@@ -1,9 +1,34 @@
-﻿namespace Labb2_DungeonCrawler.LevelElements;
+﻿using Labb2_DungeonCrawler.Events;
+namespace Labb2_DungeonCrawler.LevelElements;
 
 public abstract class LevelElement
 {
+    // The event that will notify subscribers when this element changes
+    public event EventHandler<LevelElementChangedEventArgs> ElementChanged;
+    
+    // Protected method to raise the event
+    protected virtual void OnElementChanged(LevelElementChangedEventArgs e)
+    {
+        // Make a local copy to avoid race conditions
+        EventHandler<LevelElementChangedEventArgs> handler = ElementChanged;
+        handler?.Invoke(this, e);
+    }
+    private Position _position;
+    public Position Position 
+    {
+        get
+        { return _position; }
+        set
+        {
+            if (_position != value)
+            {
+                Position oldPosition = _position;
+                _position = value;
+                //OnElementChanged(new LevelElementChangedEventArgs(oldPosition, value));
 
-    public Position Position { get; set; }
+            }
+        } 
+    }
     public Position PreviousPosition { get; set; }
     public char RepresentationAsChar { get; set; }
     public int VisionRange { get; set; }
