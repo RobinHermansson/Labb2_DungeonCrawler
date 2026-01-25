551cb4183af7a486987a98a2e9822281fdd820de
Robin Hermansson <robin.hermansson@stendahls.se>
2026-01-25 15:48:33 +0100

    Add PlayerClass repository

---
+++ DungeonCrawler.Domain/Interfaces/IPlayerClassRepository.cs
@@ -0,0 +1,6 @@
+﻿using DungeonCrawler.Domain.Entities;
+namespace DungeonCrawler.Domain.Interfaces;
+public interface IPlayerClassRepository : IRepository<PlayerClass, Guid>
+{
+    Task<PlayerClass?> GetByNameAsync(string name);
+}
